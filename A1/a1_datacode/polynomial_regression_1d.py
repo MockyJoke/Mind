1dcef34575e04bc6bb232884d6a63e762f632be4
Johnson Pan <Johnson Pan>
2016-10-03 19:43:27 -0700

    Re-Add A1

---
+++ A1/a1_datacode/polynomial_regression_1d.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import assignment1 as a1
+import numpy as np
+import matplotlib.pyplot as plt
+import pr_helper as prh
---
+++ A1/a1_datacode/pr_helper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import assignment1 as a1
+import numpy as np
+import matplotlib.pyplot as plt
+
+def GetPhi(inputFeatures,maxDegree):
+    numRows = np.size(inputFeatures,0)
+    phi = np.ones((np.size(inputFeatures,0),1))
+    for i in range(1, maxDegree + 1):
+        phi = np.append(phi,np.power(inputFeatures,i),1)
+    return phi
+
+def GetW(phi, targets):
+    w =np.dot(np.linalg.pinv(phi),targets)
+    return w
+
+def GetPredictRMS(phi,w,targets):
+    predicts= np.dot(np.transpose(w),np.transpose(phi))
+    errors=np.subtract(np.transpose(predicts),targets)
+    rms = np.sqrt(np.mean(np.power(errors,2)))
+    return rms
