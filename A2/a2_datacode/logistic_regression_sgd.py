51ff009b5e788f410d9522c8cbaa131fc3b2591e
Johnson Pan <Johnson Pan>
2016-10-20 22:13:01 -0700

    first draft, Q4.3 still needs work

--- A2/a2_datacode/logistic_regression_sgd.py
+++ A2/a2_datacode/logistic_regression_sgd.py
@@ -33,7 +33,6 @@ X2 = X[class2]
 
 
 
-
 DATA_FIG = 1
 
 # Set up the slope-intercept figure
@@ -44,63 +43,75 @@ DATA_FIG = 1
 # plt.xlabel('slope')
 # plt.ylabel('intercept')
 # plt.axis([-5, 5, -10, 0])
-
-
+ws=[]
+add=0.000000000000000000001
 for eta in eta_arr:
     # Initialize w.
     w = np.array([0.1, 0, 0])
-
+    size=np.size(X,0)
     # Error values over all iterations.
     e_all = []
-    size= np.size(X,0)
     for iter in range (0,max_iter):
-        index=np.random.randint(0,size)
-    
-        # Compute output using current w on all data X.
-        k=np.dot(X[index],w)
-        y = sps.expit(k)
-        
-        # e is the error, negative log-likelihood (Eqn 4.90)
-        e = -np.mean(np.multiply(t[index],np.log(y)) + np.multiply((1-t[index]),np.log(1-y)))
+        index=range(size)
+        np.random.shuffle(index)
+        for i in range (size):
+            # Compute output using current w on all data X.
+            k=np.dot(X[index[i]],w)
+            y = sps.expit(k)
+            
 
+            # Gradient of the error, using Eqn 4.91
+            grad_e = np.multiply((y - t[index[i]]), X[index[i]].T)
+
+            # Update w, *subtracting* a step in the error derivative since we're minimizing
+            w_old = w
+            w = w - eta*grad_e
+            
+            # Plot current separator and data.  Useful for interactive mode / debugging.
+            # plt.figure(DATA_FIG)
+            # plt.clf()
+            # plt.plot(X1[:,0],X1[:,1],'b.')
+            # plt.plot(X2[:,0],X2[:,1],'g.')
+            # a2.draw_sep(w)
+            # plt.axis([-5, 15, -10, 10])
+
+            
+            # Add next step of separator in m-b space.
+            # plt.figure(SI_FIG)
+            # a2.plot_mb(w,w_old)
+
+                
+            # Print some information.
+            #print 'epoch {0:d}, sample {3:d}, negative log-likelihood {1:.4f}, w={2}'.format(iter, e, w.T,i)
+        
+        y = sps.expit(np.dot(X,w))
+        # e is the error, negative log-likelihood (Eqn 4.90)
+        e = -np.mean(np.multiply(t,np.log(y)) + np.multiply((1-t),np.log(1-y)))
         # Add this error to the end of error vector.
         e_all.append(e)
 
-        # Gradient of the error, using Eqn 4.91
-        grad_e = np.multiply((y - t[index]), X[index].T)
-
-        # Update w, *subtracting* a step in the error derivative since we're minimizing
-        w_old = w
-        w = w - eta*grad_e
-        
-        # Plot current separator and data.  Useful for interactive mode / debugging.
-        # plt.figure(DATA_FIG)
-        # plt.clf()
-        # plt.plot(X1[:,0],X1[:,1],'b.')
-        # plt.plot(X2[:,0],X2[:,1],'g.')
-        # a2.draw_sep(w)
-        # plt.axis([-5, 15, -10, 10])
-
-        
-        # Add next step of separator in m-b space.
-        # plt.figure(SI_FIG)
-        # a2.plot_mb(w,w_old)
-
-            
-        # Print some information.
-        print 'epoch {0:d}, negative log-likelihood {1:.4f}, w={2}'.format(iter, e, w.T)
-        
         # Stop iterating if error doesn't change more than tol.
-        if iter>0:
-            if np.absolute(e-e_all[iter-1]) < tol:
-                break
-
+        # if iter>0:
+        #     if np.absolute(e-e_all[iter-1]) < tol:
+        #         break
+            
 
     # Plot error over iterations
-    #plt.figure()
-    plt.plot(e_all,label='{0:f}'.format(eta))
+    plt.figure(DATA_FIG)
+    plt.plot(e_all,label='eta={0:f}'.format(eta))
+    plt.legend()
     plt.ylabel('Negative log likelihood')
     plt.title('Training logistic regression')
     plt.xlabel('Epoch')
 
+    # plt.figure(DATA_FIG)
+    # plt.clf()
+    # plt.plot(X1[:,0],X1[:,1],'b.')
+    # plt.plot(X2[:,0],X2[:,1],'g.')
+    # a2.draw_sep(w)
+    # plt.axis([-5, 15, -10, 10])
+    # plt.show()
+    ws.append(w)
+for k in ws:
+    print k
 plt.show()
